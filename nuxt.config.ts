0af7b24cab097dbbfc25bcf88ead0c8a1c780b1c
Pierre Reynaud <pierre.reynaud@malt.com>
2025-04-04 14:40:45 +0200

    First commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    baseURL: "/test",
+  },
+  srcDir: "src",
+  ssr: true,
+  compatibilityDate: "2025-04-04",
+});
---
+++ src/pages/index.vue
@@ -0,0 +1,8 @@
+<template>Test page</template>
+
+<script setup lang="ts">
+throw createError({
+  statusCode: 500,
+  statusMessage: "Custom error",
+});
+</script>
